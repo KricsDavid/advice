9e782e9d82e6e8bef724d607de683a70dac65dbf
KricsDavid <david@kricsfalusi.hu>
2024-12-04 08:47:31 +0100

    api

--- advice/src/App.tsx
+++ advice/src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
   const [advice, setAdvice] = useState('');
+  type adviceType = {
+    id:number,
+    advice:string
+  }
 
   const fetchAdvice = async () => {
-    const response = await axios.get('https://api.adviceslip.com/advice');
-    setAdvice(response.data.slip.advice);
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json()
+    console.log(data)
+    setAdvice(data.slip.advice)
   };
 
   useEffect(() => {
@@ -17,8 +23,9 @@ const App = () => {
   return (
     <div className="app">
       <h1>Advice Generator</h1>
+      
       <div className="advice-box">
-        <p>{advice}</p>
+      <h2>{advice}</h2>
       </div>
       <button onClick={fetchAdvice} className="advice-button">Generate Advice</button>
     </div>
